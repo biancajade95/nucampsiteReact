8430b33681c598e7277003fab1b61f451689dd77
BiancaPloss <94392113+biancajade95@users.noreply.github.com>
2023-09-17 00:08:19 -0400

    Comments

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
-import './App.css';
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage.js';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import './App.css';
+
 
 function App() {
   return (
@@ -15,6 +17,10 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='contact' element={<ContactPage />} />
         <Route path='directory' element={<CampsitesDirectoryPage/>} />
+        <Route
+          path='directory/:campsiteId'
+          element={<CampsiteDetailPage />}
+        />
       </Routes>
       <Footer />
     </div>
